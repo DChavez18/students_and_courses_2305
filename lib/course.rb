7f9b0f0b2885190fefc5a701ba958558159fe039
Derek Chavez <frrenose17@yahoo.com>
2023-05-24 09:34:21 -0600

    test: write course tests

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,21 @@
 class Course
 
+  attr_reader :name,
+              :capacity,
+              :students
+  def initialize(name, capacity)
+    @name = name
+    @capacity = 2
+    @students = []
+    @full = false
+  end
+
+  def full?
+    @full
+  end
+
+  def enroll
+    
+  end
+
 end
--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -2,6 +2,29 @@ require 'rspec'
 require './lib/course'
 require './lib/student'
 
-RSpec.describe Course do 
+RSpec.describe Course do
+  before(:each) do
+    @course = Course.new("Calculus", 2)
+    @student1 = Student.new({name: "Morgan", age: 21})
+    @student2 = Student.new({name: "Jordan", age: 29})
+  end
+
+  it 'exists' do
+    expect(@course).to be_a(Course)
+  end
+
+  it 'has attributes' do
+    expect(@course.name).to eq("Calculus")
+    expect(@course.capacity).to eq(2)
+    expect(@course.students).to eq([])
+    expect(@course.full?).to eq(false)
+  end
+
+  it 'can enroll students and fill up' do
+    @course.enroll(@student1)
+    @course.enroll(@student2)
+    expect(@course.students).to eq([@student1, @student2])
+    expect(@course.full?).to eq(true)
+  end
 
 end
