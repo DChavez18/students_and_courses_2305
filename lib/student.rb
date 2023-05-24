2962740e2c779d70dcf6cb8d08af589f354f609e
Derek Chavez <frrenose17@yahoo.com>
2023-05-24 09:18:14 -0600

    test: finish student spec

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
 class Student
+  attr_reader :name,
+              :age,
+              :scores
+
+  def initialize(student_details)
+    @name = student_details[:name]
+    @age = student_details[:age]
+    @scores =[]
+  end
+
+  def log_score(score)
+    @scores << score
+  end
+
+  def grade
+    total_score = @scores.sum
+    grade = total_score.to_f/@scores.length
+  end
  
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,5 +2,26 @@ require 'rspec'
 require './lib/student'
 
 RSpec.describe Student do
+  before(:each) do
+
+    @student = Student.new({name: "Morgan", age: 21})
   
+end
+
+  it 'exists' do
+    expect(@student).to be_a(Student)
+  end
+
+  it 'has attributes' do
+    expect(@student.name).to eq("Morgan")
+    expect(@student.age).to eq(21)
+    expect(@student.scores).to eq([])
+  end
+  
+  it 'can get/log scores and be graded' do
+    @student.log_score(89)
+    @student.log_score(78)
+    expect(@student.scores).to eq([89,78])
+    expect(@student.grade).to eq(83.5)
+  end
 end
