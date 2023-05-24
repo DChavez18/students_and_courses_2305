88d584aadba02c858bdad39a1c97a0fe1b0debe8
Derek Chavez <frrenose17@yahoo.com>
2023-05-24 10:30:59 -0600

    feat: add studets_below method

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -7,6 +7,26 @@ class Gradebook
     @courses = []
   end
   
-  def add_course
+  def add_course(course)
+    @courses << course
+  end
+
+  def list_all_students
+    students_by_course = {}
+    @courses.each do |course|
+      students_by_course[course.name] = course.students.map(&:name)
+    end
+    students_by_course
+  end
+
+  def students_below
+    below_threshold_students = []
+    @courses.each do |course|
+      course.students.each do |student|
+        below_threshold_students << student.name if student.grade < 70
+      end
+    end
+    below_threshold_students
+  end
   
 end
--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Gradebook do
   before(:each) do
     @gradebook = Gradebook.new("Prof. Chavez", [])
     @course1 = Course.new(:Calculus, 2)
+    @course2 = Course.new(:CS, 3)
     @student1 = Student.new({name: "Morgan", age: 21})
     @student2 = Student.new({name: "Jordan", age: 29})
     @student3 = Student.new({name: "Felix", age: 27})
@@ -17,20 +18,26 @@ RSpec.describe Gradebook do
   end
 
   it "has attributes" do
-    @course2 = Course.new(:CS, 3)
     expect(@gradebook.instructor).to eq("Prof. Chavez")
     expect(@gradebook.courses).to eq([])
     @gradebook.add_course(@course1)
     @gradebook.add_course(@course2)
-    expect(@gradebook.courses).to eq([:Calculus, :CS])
+    expect(@gradebook.courses).to eq([@course1, @course2])
   end
 
-  xit "can list all students in courses" do
+  it "can list all students in courses" do
+    @gradebook.add_course(@course1)
+    @gradebook.add_course(@course2)
+    @course1.enroll(@student1)
+    @course1.enroll(@student2)
+    @course2.enroll(@student1)
+    @course2.enroll(@student2)
+    @course2.enroll(@student3)
     expected_data = {Calculus: ["Morgan", "Jordan"], CS: ["Morgan", "Jordan", "Felix"]}
     expect(@gradebook.list_all_students).to eq(expected_data)
   end
 
-  xit 'can check students below threshold' do
+  it 'can check students below threshold' do
     expect(@gradebook.students_below).to eq([])
   end
 end
